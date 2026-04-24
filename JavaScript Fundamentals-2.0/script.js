f120c6b0fd8589b586692cafea8b920624d36970
Hawa-codes <adediranhawa@gmail.com>
2026-04-24 01:30:17 +0100

    Java script Fundamentals

--- JavaScript Fundamentals-2.0/script.js
+++ JavaScript Fundamentals-2.0/script.js
@@ -69,3 +69,191 @@ let f = prompt("First number?", 1);
 let j = prompt("Second number?", 2);
 
 alert(+f + +j); // 3
+
+// What will be the result for these expressions?
+5 > 4  // true
+"apple" > "pineapple"  // false
+"2" > "12"  // false
+undefined == null  // true
+undefined === null // false
+null == "\n0\n"  // false
+null === +"\n0\n" // false
+
+// Will alert be shown?
+if ("0") {
+  alert( 'Hello' );
+}                    // Yes it will run because "0" is true
+
+// Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+// If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+
+let questions = prompt('What is the "official" name of JavaScript?', '');
+
+if (questions = 'ECMAScript'){
+    alert('Right!')
+}else {
+    alert('You don’t know? ECMAScript!')
+}
+
+
+// Using if..else, write the code which gets a number via prompt and then shows in alert:
+let numbers = prompt('Number', '');
+
+if (numbers > 0){
+    alert(1)
+} else if (numbers < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
+
+// Rewrite this if using the conditional operator '?':
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let resultss = (a + b < 4) ? 'Below' : 'Over';
+
+// For readability, it’s recommended to split the code into multiple lines.
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+let messaage = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+
+// What is the code below going to output?
+alert( null || 2 || undefined );  // 2
+
+alert( alert(1) || 2 || alert(3) );  // 2
+
+// What is this code going to show?
+alert( 1 && null && 2 );  // null
+
+//What will this code show?
+alert( alert(1) && alert(2) );  // 1
+
+// What will the result be?
+alert( null || 2 && 3 || 4 ); // 3
+
+//Write an if condition to check that age is between 14 and 90 inclusively.
+// “Inclusively” means that age can reach the edges 14 or 90.
+if (age >= 14 && age <= 90);
+
+// Write an if condition to check that age is NOT between 14 and 90 inclusively.
+// Create two variants: the first one using NOT !, the second one – without it.
+if (!(age >= 14 && age <= 90));
+
+// Which of these alerts are going to execute?
+if (-1 || 0) alert( 'first' );  // true
+if (-1 && 0) alert( 'second' );  // false
+if (null || -1 && 1) alert( 'third' ); // true
+
+
+let code = prompt('What is the Login', '')
+
+// What is the last value alerted by this code? Why?
+let i = 3;
+
+while (i) {
+  alert( i-- );  // 1
+}                 
+
+//Which values does the while loop show? 
+//The prefix form ++i:
+let p = 0;
+while (++p < 5) alert( p );  // 1,2,3,4
+
+//The postfix form i++
+let k = 0;
+while (k++ < 5) alert( k );  //1,2,3,4,5
+
+//The postfix form:
+for (let i = 0; i < 5; i++) alert( i );  //0,1,2,3,4
+
+//The prefix form:
+for (let i = 0; i < 5; ++i) alert( i );  //0,1,2,3,4
+
+// Use the for loop to output even numbers from 2 to 10.
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert( i );
+  }
+}
+
+//Replace "for" with "while"
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+let h = 0;
+while(h < 3){
+ alert( `number ${i}!` );
+ h++;
+}
+
+//Write the code using if..else which would correspond to the following switch:
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+// Answer 
+if(browser == 'Edge') {
+  alert("You've got the Edge!");
+} else if (browser == 'Chrome'|| browser == 'Firefox'|| browser == 'Safari'|| browser == 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+}
+
+// Rewrite the code below using a single switch statement:
+let n = +prompt('n?', '');
+
+if (n == 0) {
+  alert( 0 );
+}
+if (n == 1) {
+  alert( 1 );
+}
+
+if (n == 2 || n == 3) {
+  alert( '2,3' );
+}
+
+switch(n){
+    case 0:
+        alert( 0 );
+        break;
+    case 1:
+        alert( 1 );
+        break;
+    case 2:
+    case 3:
+        alert( '2,3' )    
+}
