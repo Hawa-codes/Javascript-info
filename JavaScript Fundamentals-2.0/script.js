9c05994a88247ee34bb2ba92230f8a8cee963665
Hawa-codes <adediranhawa@gmail.com>
2026-04-25 09:42:31 +0100

    Javascript Fundamentals-2.0

--- JavaScript Fundamentals-2.0/script.js
+++ JavaScript Fundamentals-2.0/script.js
@@ -257,3 +257,69 @@ switch(n){
     case 3:
         alert( '2,3' )    
 }
+
+// The following function returns true if the parameter age is greater than 18.
+// Otherwise it asks for a confirmation and returns its result:
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Did parents allow you?');
+  }
+}
+// Will the function work differently if else is removed?
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm('Did parents allow you?');          // NO it will not
+}
+
+//Rewrite it, to perform the same, but without if, in a single line.
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Did parents allow you?');
+  }
+}
+// ?
+function checkAge(age) {
+  return (age > 18) ? true : confirm('Did parents allow you?');
+}
+// 
+function checkAge(age) {
+  return (age > 18) || confirm('Did parents allow you?');
+}
+
+
+//Write a function min(a,b) which returns the least of two numbers a and b.
+// For instance
+min(2, 5) == 2
+min(3, -1) == -1
+min(1, 1) == 1
+
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+//Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+
+function pow(a, b) {
+  let result = 1;
+
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
+}
