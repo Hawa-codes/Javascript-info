cd2c088f5c44d072cb3ba9c164a75c71c3633334
Hawa-codes <adediranhawa@gmail.com>
2026-04-27 23:38:23 +0100

    Data types

---
+++ Data types/script.js
@@ -0,0 +1,7 @@
+// Create a script that prompts the visitor to enter two numbers and then shows their sum.
+let a = +prompt("number1?", "");
+let b = +prompt("number2?", "");
+
+alert( a + b );
+
+//
